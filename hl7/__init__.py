65e5eb46400543ac900ff62d9e0a99b077ac7850
John Paulett <john@paulett.org>
2011-07-12 21:37:09 -0500

    Adjust docstring for formatting on pypi.

--- hl7/__init__.py
+++ hl7/__init__.py
@@ -2,8 +2,8 @@
 """python-hl7 is a simple library for parsing messages of Health Level 7 
 (HL7) version 2.x into Python objects.
 
-Documentation: http://python-hl7.readthedocs.org
-Source Code: http://github.com/johnpaulett/python-hl7
+* Documentation: http://python-hl7.readthedocs.org
+* Source Code: http://github.com/johnpaulett/python-hl7
 """
 
 __version__ = '0.2.1dev'
