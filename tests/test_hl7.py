9ade70858d7093ba26cd361af2ac9740253713a8
Kevin Gill <kevin.gill@openapp.ie>
2012-03-25 21:58:09 +0100

    Verify that messages can be constructed.

--- tests/test_hl7.py
+++ tests/test_hl7.py
@@ -36,6 +36,8 @@ sample_file = u'\r'.join([
     ''
     ])
 
+SEP = '|^~\&'
+CR_SEP = '\r'
 
 class ParseTest(unittest.TestCase):
     def test_parse(self):
@@ -280,6 +282,40 @@ class ParsePlanTest(unittest.TestCase):
         n5 = n4.next()
         self.assertTrue(n5 is None)
 
+class ConstructionTest(unittest.TestCase):
+
+    def test_create_msg(self):
+        # Create a message
+        MSH = hl7.Segment(SEP[0], [hl7.Field(SEP[1], ['MSH'])])
+        MSA = hl7.Segment(SEP[0], [hl7.Field(SEP[1], ['MSA'])])
+        response = hl7.Message(CR_SEP, [MSH, MSA])
+        response['MSH.F1.R1'] = SEP[0]
+        response['MSH.F2.R1'] = SEP[1:]
+        self.assertEqual(unicode(response), u'MSH|^~\\&|\rMSA')
+
+    def test_append(self):
+        # Append a segment to a message
+        MSH = hl7.Segment(SEP[0], [hl7.Field(SEP[1], ['MSH'])])
+        response = hl7.Message(CR_SEP, [MSH])
+        response['MSH.F1.R1'] = SEP[0]
+        response['MSH.F2.R1'] = SEP[1:]
+        MSA = hl7.Segment(SEP[0], [hl7.Field(SEP[1], ['MSA'])])
+        response.append(MSA)
+        self.assertEqual(unicode(response), u'MSH|^~\\&|\rMSA')
+
+    def test_append_from_source(self):
+        # Copy a segment between messages
+        MSH = hl7.Segment(SEP[0], [hl7.Field(SEP[1], ['MSH'])])
+        MSA = hl7.Segment(SEP[0], [hl7.Field(SEP[1], ['MSA'])])
+        response = hl7.Message(CR_SEP, [MSH, MSA])
+        response['MSH.F1.R1'] = SEP[0]
+        response['MSH.F2.R1'] = SEP[1:]
+        self.assertEqual(unicode(response), u'MSH|^~\\&|\rMSA')
+        src_msg = hl7.parse(rep_sample_hl7)
+        PID = src_msg['PID'][0]
+        self.assertEqual(unicode(PID), u'PID|Field1|Component1^Component2|Component1^Sub-Component1&Sub-Component2^Component3|Repeat1~Repeat2')
+        response.append(PID)
+        self.assertEqual(unicode(response), u'MSH|^~\\&|\rMSA\rPID|Field1|Component1^Component2|Component1^Sub-Component1&Sub-Component2^Component3|Repeat1~Repeat2')
 
 if __name__ == '__main__':
     unittest.main()
