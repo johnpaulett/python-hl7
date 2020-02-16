6efbcce8a20b8eb427528fc875ec66c241f9fca8
John Paulett <john@paulett.org>
2020-02-16 10:16:00 -0500

    Bump version to 0.4.0-dev

--- hl7/version.py
+++ hl7/version.py
@@ -6,7 +6,7 @@ Primary version number source.
 Forth element can be 'dev' < 'a' < 'b' < 'rc' < 'final'. An empty 4th
 element is equivalent to 'final'.
 """
-VERSION = (0, 3, 4, 'final')
+VERSION = (0, 4, 0, 'dev')
 
 
 def get_version():
