49522800595d20a4fe20d84950ee328941eb5ad9
Joseph Wortmann <joseph.wortmann@gmail.com>
2020-08-21 10:14:53 -0500

    black formatting

--- hl7/asyncio/__init__.py
+++ hl7/asyncio/__init__.py
@@ -1,15 +1,15 @@
 from .exceptions import InvalidBlockError
 from .streams import (
-    open_hl7_connection, 
+    open_hl7_connection,
     start_hl7_server,
     HL7StreamReader,
-    HL7StreamWriter
+    HL7StreamWriter,
 )
 
 __all__ = [
-    'open_hl7_connection',
-    'start_hl7_server',
-    'HL7StreamReader',
-    'HL7StreamWriter',
-    'InvalidBlockError'
+    "open_hl7_connection",
+    "start_hl7_server",
+    "HL7StreamReader",
+    "HL7StreamWriter",
+    "InvalidBlockError",
 ]
